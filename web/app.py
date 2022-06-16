3455f86da170852251e486d7361302758c3f4e08
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-06-17 08:24:02 +0900

    web

--- web/app.py
+++ web/app.py
@@ -1,18 +1,44 @@
+from os.path import join
+from shutil import move
+from urllib.parse import quote, unquote
+
 from flask import Flask, render_template, redirect, request
+
+from generate2 import IMG_EXTENSIONS, main
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    error = unquote(request.args.get('error', ''))
+    return render_template('index.html', error=error)
+
 
 @app.route('/emoji/', methods=['POST'])
 def emoji():
-    if request.method != 'POST':
-        return redirect('/')
-    # код генерации эмодзи
-    # эмодзи нужно сохранить в папку static
-    filename = 'emoji.webp'
+    # Извлекаем первый файл
+    file = next(iter(request.files.values()))
+    filename = file.filename.lower()
+    for ext in IMG_EXTENSIONS:
+        if file.filename.endswith(ext):
+            src = join('data', 'GG', filename)
+            dst = join('experiments', 'name', 'results', 'pred', filename)
+            break
+    else:
+        error = quote('Invalid file format')
+        return redirect(f'/?error={error}')
+
+    # Сохраняем куда нужно
+    file.save(src)
+    
+    # Запускаем генерацию
+    main()
+    
+    # Перемещаем исходный файл и результат
+    move(src, join('static', 'inp_' + filename))
+    move(dst, join('static', 'out_' + filename))
     return render_template('emoji.html', filename=filename)
+
 
 app.run(debug=True)
